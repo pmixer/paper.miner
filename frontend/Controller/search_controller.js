119146c90e980e214226cb263742252eb36a4df0
Jessie Liu <jliu808@gatech.edu>
2020-04-22 17:50:41 -0400

    update frontend

--- frontend/Controller/search_controller.js
+++ frontend/Controller/search_controller.js
@@ -1,7 +1,7 @@
 // let async = require('async');
 
-// const { body,validationResult, query } = require('express-validator/check');
-// const { sanitizeBody, sanitizeQuery } = require('express-validator/filter');
+const { body,validationResult, query } = require('express-validator/check');
+const { sanitizeBody, sanitizeQuery } = require('express-validator/filter');
 
 //const e = document.getElementById('1');
 
@@ -26,8 +26,18 @@
 exports.search = [
     
     (req, res, next) => {
-        res.render('search_engine');
+        res.render('search');
         
+    }
+
+];
+
+exports.search_post = [
+    (req, res, next) => {
+        var search_content = req.body.search_content;
+        var limit = req.body.limit;
+        
+        var url = "http://127.0.0.1:5000/get_author"
     }
 
 ];
